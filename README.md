5988b339a452292b25f967f516b146b8c644bf38
gumipo <10sei0otto.22@gmail.com>
2020-12-12 16:50:39 +0900

    create-environment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # svelte-voting-app
+
+## 環境構築
+
+npx degit sveltejs/template my-project
+
+npm install して node_modules を追加
+
+npm run dev 　ローカルサーバー起動
---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	let name = "ねこ";
+	let beltColor = "pink";
+
+	const handleClick = () => {
+		beltColor = "blue";
+	};
+
+	//bind:valueで使わない
+	const handleInput = (e) => {
+		name = e.target.value;
+	};
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<main>
+	<h1 style="color : {beltColor}">{name}</h1>
+	<p>{beltColor} belt</p>
+	<button on:click={handleClick}>update-color</button>
+	<!-- <input type="text" on:input={handleInput} value={name} /> -->
+	<input type="text" bind:value={name} />
+</main>
